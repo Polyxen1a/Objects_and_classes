5f0b2688e37945708060e56b4a55c61fdaf658bb
Polyxen1a <113852536+Polyxen1a@users.noreply.github.com>
2022-11-03 23:06:39 +0300

    Revert "Objects_and_classes"

--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        Author stephenKing = new Author("Stephen", "King");
-        Book firstBook = new Book("Desperation", stephenKing, 1997);
-
-        Book secondBook = new Book("Effective java", new Author("Joshua", "Bloch"), 2015);
-        firstBook.setPublishYear(2020);
-        System.out.println(firstBook.getPublishYear());
-
-
-        System.out.println("Authors");
-        Author stephenKing1 = new Author("Stephen", "King");
-        Author stephenKing2 = new Author("Stephen", "King");
-        Author levTolstoy = new Author("Lev", "Tolstoy");
-        System.out.println("Authors copies equal: " + stephenKing.equals(stephenKing2));
-        System.out.println("Authors copies equal by hashCode: " + (stephenKing.hashCode() == stephenKing2.hashCode()));
-        System.out.println("Authors equal: " + stephenKing.equals(levTolstoy));
-        System.out.println("Authors equal by hashCode: " + (stephenKing.hashCode() == levTolstoy.hashCode()));
-
-        System.out.println(stephenKing);
-        System.out.println(levTolstoy);
-
-        System.out.println("======================");
+        System.out.println("Hello world!");
     }
 }
